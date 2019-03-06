04f957e3dbafa8ceba2e6608bfbc2706021bbeaf
Sophie Hope <bethmcdermid522@gmail.com>
2019-03-06 11:08:10 +0000

    added more templates

--- WER_app/urls.py
+++ WER_app/urls.py
@@ -4,6 +4,9 @@ from WER_app import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^search/', views.search, name='search'),
-    url(r'^review_sample/', views.review_sample, name='review_sample')
-    
+    url(r'^review_sample/', views.review_sample, name='review_sample'),
+    url(r'^about/', views.about, name='about'),
+    url(r'^FAQ/', views.FAQ, name='FAQ'),
+    url(r'^contact-us/', views.contact, name='contact-us'),
+    url(r'^t&cs/', views.tAndC, name='t&cs'),
 ]
--- WER_app/views.py
+++ WER_app/views.py
@@ -8,7 +8,19 @@ def index(request):
 
 def search(request):
     return render(request, 'WER_app/search.html')
+
+def about(request):
+    return render(request, 'WER_app/about.html')
     
+    
+def FAQ(request):
+    return render(request, 'WER_app/FAQ.html')
+
+def tAndC(request):
+    return render(request, 'WER_app/t&cs.html')
+    
+def contact(request):
+    return render(request, 'WER_app/contact-us.html')
     
 def review_sample(request):
     review_list = Review.objects.order_by('reviewID')[:4] 
