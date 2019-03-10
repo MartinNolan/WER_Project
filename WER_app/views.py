b1b703005b8c3c5a4f6f44d8e25dd34bc5b373cf
Martin Nolan <2313202n@student.gla.ac.uk>
2019-03-10 18:02:26 +0000

    .

--- WER_app/views.py
+++ WER_app/views.py
@@ -1,13 +1,15 @@
-<<<<<<< HEAD
 from django.shortcuts import render
 from django.http import HttpResponse
 from WER_app.models import Review, Page
-from WER_app.forms import UserForm, UserProfileForm
+from WER_app.forms import UserForm, UserProfileForm
+
 from django.contrib.auth import authenticate, login
 from django.http import HttpResponseRedirect, HttpResponse
-from django.core.urlresolvers import reverse
+from django.core.urlresolvers import reverse
+
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth import logout
+from django.contrib.auth import logout
+
 
 def index(request):
     return render(request, 'WER_app/index.html')
@@ -18,7 +20,24 @@ def search(request):
 
 def about(request):
     return render(request, 'WER_app/about.html')
-    
+
+def review(request, page_name_slug):
+    #review_list = Review.objects.order_by('reviewID')[:4] 
+    #pages = Page.objects.order_by('title')[:4]
+    #context_dict = {'reviews': review_list, 'pages':pages}
+    context_dict = {}
+    try:
+        page = Page.objects.get(slug=page_name_slug)
+        pages = Page.objects.filter(title=page.title)
+        reviews = Review.objects.filter(title=page.title)
+        context_dict['pages'] = pages
+        context_dict['page'] = page
+        context_dict['reviews'] = reviews
+    except Page.DoesNotExist:
+        context_dict['page'] = None
+        context_dict['pages'] = None 
+
+    return render(request, 'WER_app/review.html', context_dict)    
     
 def FAQ(request):
     return render(request, 'WER_app/FAQ.html')
@@ -102,57 +121,3 @@ def user_logout(request):
 	logout(request)
 
 	return HttpResponseRedirect(reverse('index'))
-
-    
-=======
-from django.shortcuts import render
-from django.http import HttpResponse
-from WER_app.models import Review, Page
-
-def index(request):
-    return render(request, 'WER_app/index.html')
-
-
-def search(request):
-    return render(request, 'WER_app/search.html')
-
-def about(request):
-    return render(request, 'WER_app/about.html')
-    
-    
-def FAQ(request):
-    return render(request, 'WER_app/FAQ.html')
-
-def tAndC(request):
-    return render(request, 'WER_app/t&cs.html')
-    
-def contact(request):
-    return render(request, 'WER_app/contact-us.html')
-    
-def review(request, page_name_slug):
-    #review_list = Review.objects.order_by('reviewID')[:4] 
-    #pages = Page.objects.order_by('title')[:4]
-    #context_dict = {'reviews': review_list, 'pages':pages}
-    context_dict = {}
-    try:
-        page = Page.objects.get(slug=page_name_slug)
-        pages = Page.objects.filter(title=page.title)
-        reviews = Review.objects.filter(title=page.title)
-        context_dict['pages'] = pages
-        context_dict['page'] = page
-        context_dict['reviews'] = reviews
-    except Page.DoesNotExist:
-        context_dict['page'] = None
-        context_dict['pages'] = None
-        
-
-    return render(request, 'WER_app/review.html', context_dict)
-
-def review_sample(request):
-    review_list = Review.objects.order_by('reviewID')[:4] 
-    pages = Page.objects.order_by('title')[:4]
-    context_dict = {'reviews': review_list, 'pages':pages}
-
-    return render(request, 'WER_app/review_sample.html', context_dict)
-
->>>>>>> 9966d4de5141d7bd1e73dc015bfb14da423884d8
