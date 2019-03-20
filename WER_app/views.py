615cce086c793f5477843b3ec4f14219ca908020
Martin Nolan <2313202n@student.gla.ac.uk>
2019-03-20 20:40:38 +0000

    login page removed and cookies added

--- WER_app/views.py
+++ WER_app/views.py
@@ -12,13 +12,19 @@ from datetime import datetime
 
 
 def index(request):
-    return render(request, 'WER_app/index.html')
+    request.session.set_test_cookie()
+    visitor_cookie_handler(request)
+    response = render(request, 'WER_app/index.html',)
+    return response
 
 
 def search(request):
     return render(request, 'WER_app/search.html')
 
 def about(request):
+    if request.session.test_cookie_worked():
+        print("TEST COOKIE WORKED!")
+        request.session.delete_test_cookie()
     return render(request, 'WER_app/about.html')
     
     
@@ -158,3 +164,31 @@ def offCampus(request):
     context_dict = {'reviews': review_list, 'pages':pages}
 
     return render(request, 'WER_app/offCampus.html', context_dict)
+
+
+# A helper method
+def get_server_side_cookie(request, cookie, default_val=None):
+    val = request.session.get(cookie)
+    if not val:
+        val = default_val
+    return val
+
+# Updated the function definition
+def visitor_cookie_handler(request):
+    visits = int(get_server_side_cookie(request, 'visits', '1'))
+    last_visit_cookie = get_server_side_cookie(request,
+                                               'last_visit',
+                                               str(datetime.now()))
+    last_visit_time = datetime.strptime(last_visit_cookie[:-7],
+                                        '%Y-%m-%d %H:%M:%S')
+# If it's been more than a day since the last visit...
+    if (datetime.now() - last_visit_time).days > 0:
+        visits = visits + 1
+    #update the last visit cookie now that we have updated the count
+        request.session['last_visit'] = str(datetime.now())
+    else:
+        # set the last visit cookie
+            request.session['last_visit'] = last_visit_cookie
+    # Update/set the visits cookie
+    request.session['visits'] = visits
+
